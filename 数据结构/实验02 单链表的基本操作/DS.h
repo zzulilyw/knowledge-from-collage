75f24415a881f77e9240e9e84340ab03c776ff0e
zzuli_lyw <378305181@qq.com>
2019-05-07 17:34:11 +0800

    Create DS.h

---
+++ 数据结构/实验02 单链表的基本操作/DS.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <bits/stdc++.h>
+
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+
+typedef int Status;
+
+
